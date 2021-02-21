c493caa58b8ca7960b431662fa4c4716f25b4410
Alex Nielsen <alex.nielsen@gmail.com>
2021-02-20 19:30:51 -0500

    query string check

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,6 +1,8 @@
-exports.handler = function (event, context, callback) {
-  callback(null, {
+exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || "World";
+  console.log(event.queryStringParameters);
+  return {
     statusCode: 200,
-    body: "Hello, World"
-  });
+    body: `Hello, ${name}`
+  };
 };
